8e099aebc8aec2e94928ad8eac2f125d9b911527
Eugene <Zhekul.90@gmail.com>
2020-09-03 13:20:20 +0300

    Level_1

--- Level_3/lesson_1/src/main/java/project/by/kulinich/Box.java
+++ Level_3/lesson_1/src/main/java/project/by/kulinich/Box.java
@@ -7,19 +7,26 @@ import java.util.Arrays;
 
 public class Box<T extends Fruit> {
     private T[] obj;
+    ArrayList<T> arrayList = new ArrayList<T>();
 
-    public Box(T ... obj) {
+    public Box(T... obj) {
         this.obj = obj;
-        ArrayList<T> arrayList = new ArrayList<T>(Arrays.asList(obj));
+        arrayList.addAll(Arrays.asList(obj));
+    }
+
+    public ArrayList<T> putInBox(T obj) {
+        arrayList.add(obj);
+        return arrayList;
     }
 
     public float getWeight(Box box) {
         float weight = 0.0f;
-
+/*
         for (T o: box
              ) {
-            weight += o.getWeight;
+            weight += o.getWeight; //не видит getWeight
         }
+*/
         return weight;
     }
 }
